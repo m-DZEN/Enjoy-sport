88e4e51a36719b86e8caad12fcf83743319a2180
Irina <irina.danilenko@gmail.com>
2023-02-07 17:44:13 +0300

    settings

--- client/src/components/Settings/Settings.jsx
+++ client/src/components/Settings/Settings.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React from "react";
 
 export default function Settings() {
   return (
-    <div>Settings</div>
-  )
+    <>
+      <div>
+        <div>
+          <h3 style={{ color: "red" }}>Введите свои данные</h3>
+        </div>
+        <div>
+          <div>
+            <label>
+              Login
+              <input />
+            </label>
+          </div>
+          <div>
+            <label>
+              password
+              <input />
+            </label>
+          </div>
+          <div>
+            <label>
+              email
+              <input />
+            </label>
+          </div>
+          <div>
+            <label>
+              дата рождения
+              <input />
+            </label>
+          </div>
+        </div>
+        <div>
+          <button>SAVE</button>
+        </div>
+      </div>
+    </>
+  );
 }
-
