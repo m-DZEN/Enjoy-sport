7967ef31f9c78b884e3647691e205d4e8bf7a537
Vladimir Solo <freeman.wushu@mail.ru>
2023-02-08 00:11:59 +0600

    create traning

--- client/src/components/Training/Training.jsx
+++ client/src/components/Training/Training.jsx
@@ -1,8 +1,93 @@
-import React from 'react'
+import React from "react";
+import "./Training.css";
+import { Link } from "react-router-dom";
 
 export default function Training() {
   return (
-    <div>Training</div>
-  )
+    <div className="container">
+      <div>
+        <button className="btn-show">Понедельник</button>
+        <div className="block-none">
+          <p>Понедельник</p>
+          <Link to="/workout">
+            <button>Тренировки</button>
+          </Link>
+          <Link to="/nutrition">
+            <button>Питание</button>
+          </Link>
+        </div>
+      </div>
+      <div>
+        <button className="btn-show">Вторник</button>
+        <div className="block-none">
+          <p>Вторник</p>
+          <Link to="/workout">
+            <button>Тренировки</button>
+          </Link>
+          <Link to="/nutrition">
+            <button>Питание</button>
+          </Link>
+        </div>
+      </div>
+      <div>
+        <button className="btn-show">Среда</button>
+        <div className="block-none">
+          <p>Среда</p>
+          <Link to="/workout">
+            <button>Тренировки</button>
+          </Link>
+          <Link to="/nutrition">
+            <button>Питание</button>
+          </Link>
+        </div>
+      </div>
+      <div>
+        <button className="btn-show">Четверг</button>
+        <div className="block-none">
+          <Link to="/workout">
+            <button>Тренировки</button>
+          </Link>
+          <Link to="/nutrition">
+            <button>Питание</button>
+          </Link>
+        </div>
+      </div>
+      <div>
+        <button className="btn-show">Пятница</button>
+        <div className="block-none">
+          <p>Пятница</p>
+          <Link to="/workout">
+            <button>Тренировки</button>
+          </Link>
+          <Link to="/nutrition">
+            <button>Питание</button>
+          </Link>
+        </div>
+      </div>
+      <div>
+        <button className="btn-show">Суббота</button>
+        <div className="block-none">
+          <p>Суббота</p>
+          <Link to="/workout">
+            <button>Тренировки</button>
+          </Link>
+          <Link to="/nutrition">
+            <button>Питание</button>
+          </Link>
+        </div>
+      </div>
+      <div>
+        <button className="btn-show">Воскресенье</button>
+        <div className="block-none">
+          <p>Воскресенье</p>
+          <Link to="/workout">
+            <button>Тренировки</button>
+          </Link>
+          <Link to="/nutrition">
+            <button>Питание</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
-
