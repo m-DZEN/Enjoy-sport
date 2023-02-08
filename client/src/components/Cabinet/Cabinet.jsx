a3705b73fef943df3262b8c21834f749b6f5f022
Irina <irina.danilenko@gmail.com>
2023-02-08 22:23:33 +0300

    dataSettings

--- client/src/components/Cabinet/Cabinet.jsx
+++ client/src/components/Cabinet/Cabinet.jsx
@@ -1,110 +1,213 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useSelector } from "react-redux";
 import ButtonChatAndMotivation from "../ButtonChatAndMotivation/ButtonChatAndMotivation";
 import "./Cabinet.css";
 
 export default function Cabinet() {
+  const user = useSelector((store) => store.userStore);
+  console.log(user.userId);
+
+  // useEffect(() => {
+  //   (const res = await fetch("http://localhost:3001/cabinet", {
+  //     method: "PUT",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({ inputs, user }),
+  //     credentials: "include",
+  //   }))()
+  // }, [])
+  
+  const initState = {
+    burthday: "",
+    height: "",
+    weight: "",
+    gender: "female",
+    bodyType: "full",
+    typeProgram: "loseWeight",
+    finishWeight: "",
+    ready: "db",
+    notReady: "db",
+    contra: "db",
+  };
+
+  const [inputs, setInputs] = useState(initState);
+
+  const formHandler = (e) => {
+    console.log(e.target.name, e.target.value);
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const createUserData = async (e) => {
+    e.preventDefault();
+    const res = await fetch("http://localhost:3001/cabinet", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ inputs, user }),
+      credentials: "include",
+    });
+    const data = await res.json();
+    console.log("data", data);
+    setInputs(initState);
+  };
   return (
     <>
       <div>
         <div>
-          <h2 style={{ color: "red" }}>введите свои данные</h2>
+          <h2 style={{ color: "blue" }}>введите свои данные</h2>
         </div>
-        <div className="inputDiv">
-          <div>
-            <label>
-              дата рождения
-              <input />
-            </label>
-          </div>
-          <div>
-            <label>
-              рост
-              <input />
-            </label>
-          </div>
-          <div>
-            <label>
-              вес
-              <input />
-            </label>
-          </div>
-          <div>
-            <label>
-              пол
-              <select>
-                <option>мужской</option>
-                <option>женский</option>
-              </select>
-            </label>
-          </div>
-          <div>
-            <label>
-              телосложение
-              <select>
-                <option>худощавое</option>
-                <option>полное</option>
-              </select>
-            </label>
-          </div>
-        </div>
-        <div className="mainTrain">
-          <div>
-            <h3 style={{ color: "red" }}>выберите программу тренировок</h3>
-          </div>
-          <div>
-            <div>   
-            <label>
-                цель
-              <select>
-                <option>похудеть</option>
-                <option>набрать вес</option>
-                <option>удержать вес</option>
-              </select>
+        <form onSubmit={createUserData}>
+          <div className="inputDiv">
+            <div>
+              <label>
+                дата рождения
+                <input
+                  value={inputs.burthday}
+                  required
+                  onChange={formHandler}
+                  type="date"
+                  name="burthday"
+                />
               </label>
-                {/* <input /> */}
             </div>
             <div>
               <label>
-                желаемый вес
-                <input />
+                рост
+                <input
+                  value={inputs.height}
+                  required
+                  onChange={formHandler}
+                  min="100"
+                  max="200"
+                  type="number"
+                  name="height"
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                вес
+                <input
+                  value={inputs.weight}
+                  required
+                  onChange={formHandler}
+                  min="40"
+                  max="140"
+                  type="number"
+                  name="weight"
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                пол
+                <select
+                  value={inputs.gender}
+                  required
+                  onChange={formHandler}
+                  name="gender"
+                >
+                  <option value="female">женский</option>
+                  <option value="male">мужской</option>
+                </select>
+              </label>
+            </div>
+            <div>
+              <label>
+                телосложение
+                <select
+                  value={inputs.bodyType}
+                  required
+                  onChange={formHandler}
+                  name="bodyType"
+                >
+                  <option value="full">полное</option>
+                  <option value="thin">худощавое</option>
+                </select>
               </label>
             </div>
           </div>
-        </div>
-        <h3 style={{ color: "red" }}>Список продуктов</h3>
-        <div className="products">
-          <div className="ol">
-            <p>ем с удовольствием</p>
-            <ol>
-              <li>
-                <input />
-              </li>
-              <li>
-                <input />
-              </li>
-            </ol>
+          <div className="mainTrain">
+            <div>
+              <h3 style={{ color: "blue" }}>выберите программу тренировок</h3>
+            </div>
+            <div>
+              <div>
+                <label>
+                  цель
+                  <select
+                    value={inputs.typeProgram}
+                    onChange={formHandler}
+                    name="typeProgram"
+                  >
+                    <option value="loseWeight">похудеть</option>
+                    <option value="gainWeight">набрать вес</option>
+                    <option value="holdWeight">удержать вес</option>
+                  </select>
+                </label>
+                {/* <input /> */}
+              </div>
+              <div>
+                <label>
+                  желаемый вес
+                  <input
+                    value={inputs.finishWeight}
+                    required
+                    type="number"
+                    onChange={formHandler}
+                    name="finishWeight"
+                  />
+                </label>
+              </div>
+            </div>
           </div>
-          <div className="ol">
-            <p>аллергия</p>
-            <ol>
-              <li>
-                <input />
-              </li>
-              <li>
-                <input />
-              </li>
-            </ol>
+
+          <h3 style={{ color: "blue" }}>Список продуктов</h3>
+          <div className="products">
+            <div className="ol">
+              <p>готов отказаться</p>
+              <textarea
+                value={inputs.ready}
+                required
+                onChange={formHandler}
+                name="ready"
+              ></textarea>
+            </div>
+            <div className="ol">
+              <p>не готов отказаться</p>
+              <textarea
+                value={inputs.notReady}
+                required
+                onChange={formHandler}
+                name="notReady"
+              ></textarea>
+            </div>
           </div>
-        </div>
+          <div>
+            <div>
+              <p>Противопоказания</p>
+              <textarea
+                value={inputs.contra}
+                required
+                onChange={formHandler}
+                name="contra"
+              ></textarea>{" "}
+            </div>
+          </div>
+          <div>
+            <button type="submit" style={{ color: "red" }}>
+              Сохранить
+            </button>
+          </div>
+        </form>
         <div>
-          <div>
-            <button>Противопоказания</button>
-          </div>
-          <div>
-            <button>Сохранить</button>
-          </div>
-        </div>
+
         <ButtonChatAndMotivation />
+        </div>
       </div>
     </>
   );
---
+++ server/src/controllers/createUserDataControllers.js
@@ -0,0 +1,37 @@
+const { User } = require('../../db/models');
+
+const userData = async (res, req) => {
+  
+}
+
+const createUserData = async (req, res) => {
+  // console.log('req.body ===>', req.body);
+  const { inputs } = req.body;
+  const { user } = req.body;
+  // console.log('---------->', user.userId);
+  // console.log('inputs---------->', inputs);
+
+  try {
+    const userData = await User.update({
+      burthday: inputs.burthday,
+      height: inputs.height,
+      weight: inputs.weight,
+      gender: inputs.gender,
+      body_type: inputs.bodyType,
+      type_program: inputs.typeProgram,
+    final_weight: inputs.finishWeight,
+    ready: inputs.ready,
+    notready: inputs.notReady,
+    contra: inputs.contra,
+    }, {
+      where: { id: user.userId },
+    });
+    res.json(userData);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
+};
+
+module.exports = { userData, 
+  createUserData };
