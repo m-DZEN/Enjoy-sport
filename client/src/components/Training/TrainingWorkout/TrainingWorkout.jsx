baa61d7a336dad5e312c0f099f45f12f679f59e2
Max_V <mdzen87@gmail.com>
2023-02-09 20:30:35 +0300

    training style

--- client/src/components/Training/TrainingWorkout/TrainingWorkout.jsx
+++ client/src/components/Training/TrainingWorkout/TrainingWorkout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './TrainingWorkout.css';
 
 export default function TrainingWorkout() {
-  /*   const days = [
+  const days = [
     { id: 1, title: 'Понедельник' },
     { id: 2, title: 'Вторник' },
     { id: 3, title: 'Среда' },
@@ -10,63 +10,63 @@ export default function TrainingWorkout() {
     { id: 2, title: 'Пятница' },
     { id: 2, title: 'Суббота' },
     { id: 2, title: 'Воскресенье' },
-  ]; */
-
+  ];
+  const dailyTrains = [
+    {
+      id: 1, title: 'Жим штанги лёжа', weight: 50, sets: 3, rep: 10, rest: 90,
+    },
+    {
+      id: 3, title: 'Жим гантелей лежа', weight: 10, sets: 4, rep: 8, rest: 90,
+    },
+    {
+      id: 5, title: 'Отжимания от пола', weight: 0, sets: 3, rep: 10, rest: 90,
+    },
+  ];
   return (
     <>
       <div className="workout_container">
-        <table>
-          <caption>Понедельник</caption>
+        <caption>{days[0].title}</caption>
+        <div className="dailyTrain">
+          {dailyTrains.map((el) => (
+            <div className="dailyBox">
+              <div>
+                {' '}
+                {el.title}
+                {' '}
+              </div>
+              <div>
+                {' '}
+                Вес
+                {' '}
+                {el.weight}
+                {' '}
+                кг
+                {' '}
+              </div>
+              <div>
+                {' '}
+                Подходов
+                {el.sets}
+                {' '}
+              </div>
+              <div>
+                {' '}
+                Повторений
+                {' '}
+                {el.rep}
+                {' '}
+              </div>
+              <div>
+                {' '}
+                Отдых
+                {' '}
+                {el.rest}
+                {' '}
+              </div>
+            </div>
+          ))}
+        </div>
 
-          <thead>
-            <tr>
-              <td>
-                Название
-              </td>
-              <th id="stud" scope="col">
-                Подходы
-              </th>
-              <th id="chal" scope="col">
-                Повторения
-              </th>
-              <th id="villa" scope="col">
-                Выполнено
-              </th>
-              <th id="villa" scope="col">
-                Подробнее
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th id="par" className="span" colSpan="5" scope="colgroup">
-                Дневная
-              </th>
-            </tr>
-            <tr>
-              <th headers="par" id="pbed1">
-                Приседание
-              </th>
-              <td headers="par pbed1 stud">3</td>
-              <td headers="par pbed1 apt">20, 30, 35</td>
-              <td headers="par pbed1 chal">
-                <input type="checkbox" name="" id="" />
-              </td>
-              <td headers="par pbed2 villa"><a href="https://www.youtube.com/">Youtube</a></td>
-            </tr>
-            <tr>
-              <th headers="par" id="pbed2">
-                Отжимание
-              </th>
-              <td headers="par pbed2 stud">3</td>
-              <td headers="par pbed2 apt">20, 30, 35</td>
-              <td headers="par pbed1 chal">
-                <input type="checkbox" name="" id="" />
-              </td>
-              <td headers="par pbed2 villa"><a href="https://www.youtube.com/">Youtube</a></td>
-            </tr>
-          </tbody>
-        </table>
       </div>
       <button className="workout_btn" type="submit">Завершить</button>
 
