a4969ccfdf1ed4100157dfbaabdcdd6fd37dbd07
Max_V <mdzen87@gmail.com>
2023-02-09 19:44:39 +0300

    my day styles

--- client/src/components/Training/Training.jsx
+++ client/src/components/Training/Training.jsx
@@ -4,22 +4,25 @@ import { Link } from 'react-router-dom';
 
 export default function Training() {
   const days = [
-    { title: 'Понедельник' },
-    { title: 'Вторник' },
-    { title: 'Среда' },
-    { title: 'Четверг' },
-    { title: 'Пятница' },
-    { title: 'Суббота' },
-    { title: 'Воскресенье' },
+    { id: 1, title: 'Понедельник' },
+    { id: 2, title: 'Вторник' },
+    { id: 3, title: 'Среда' },
+    { id: 2, title: 'Четверг' },
+    { id: 2, title: 'Пятница' },
+    { id: 2, title: 'Суббота' },
+    { id: 2, title: 'Воскресенье' },
   ];
+
   return (
     <div className="workout_container">
       {days.map((el) => (
         <div className="block">
-          <div className="btn-show">{el.title}</div>
+          <div className="block-show" key={el.id}>{el.title}</div>
           <div className="block-none">
-            <Link className="link" to="/workout"> Тренировки</Link>
-            <Link className="link" to="/nutrition">Питание</Link>
+            {/* Сделать бы параметрический запрос
+            <Link className="linkButton" to={`/workout/${el.id}`}> Тренировки</Link> */}
+            <Link className="linkButton" to="/workout"> Тренировки</Link>
+            <Link className="linkButton" to="/nutrition">Питание</Link>
           </div>
         </div>
       ))}
