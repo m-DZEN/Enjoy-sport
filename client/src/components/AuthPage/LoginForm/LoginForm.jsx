07bac0326be02419943fdc682c8067a7b48c9a7a
Pavel Urseev <urseev.pavel@gmail.com>
2023-02-10 19:31:16 +0300

    auth new

--- client/src/components/AuthPage/LoginForm/LoginForm.jsx
+++ client/src/components/AuthPage/LoginForm/LoginForm.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
+import styles from './LoginForm.module.scss';
 
 export default function LoginForm({ setIsAlreadyRegistered }) {
   return (
-    <div>
-      <div style={{ color: 'white' }}>LoginForm</div>
-      <button type="button" onClick={() => setIsAlreadyRegistered((prev) => !prev)}>REGISTER</button>
+    <div className={styles.form}>
+      <h3 className={styles.formLogo}>Вход</h3>
+
+      <form
+        className={styles.formBlock}
+        onSubmit={() => console.log('SUBMIT')}
+      >
+        <input
+          className={styles.formBlockInput}
+          type="text"
+          name="login"
+          // value={authFormInput.login}
+          // onChange={handleAuthFormInputChange}
+          placeholder="Ваш логин..."
+          required
+        />
+        <input
+          className={styles.formBlockInput}
+          type="password"
+          name="password"
+          // value={authFormInput.password}
+          // onChange={handleAuthFormInputChange}
+          placeholder="Ваш пароль..."
+          required
+        />
+        <button
+          className={styles.formBlockButton}
+          type="submit"
+        >
+          Войти
+        </button>
+      </form>
+
+      <div className={styles.fillBlock} />
+
+      <button
+        className={styles.formFutterButton}
+        type="button"
+        onClick={() => setIsAlreadyRegistered((prev) => !prev)}
+      >
+        Зарегистрироваться?
+      </button>
     </div>
   );
 }
--- client/src/components/AuthPage/RegisterForm/RegisterForm.jsx
+++ client/src/components/AuthPage/RegisterForm/RegisterForm.jsx
@@ -1,10 +1,60 @@
 import React from 'react';
+import styles from './RegisterForm.module.scss';
 
 export default function RegisterForm({ setIsAlreadyRegistered }) {
   return (
-    <div>
-      <div style={{ color: 'white' }}>LoginForm</div>
-      <button type="button" onClick={() => setIsAlreadyRegistered((prev) => !prev)}>LOGIN</button>
+    <div className={styles.form}>
+      <h3 className={styles.formLogo}>Регистрация</h3>
+
+      <form
+        className={styles.formBlock}
+        onSubmit={() => console.log('SUBMIT')}
+      >
+        <input
+          className={styles.formBlockInput}
+          type="text"
+          name="login"
+          placeholder="Ваш логин..."
+          required
+        />
+        <input
+          className={styles.formBlockInput}
+          type="email"
+          name="email"
+          placeholder="Ваш email..."
+          required
+        />
+        <input
+          className={styles.formBlockInput}
+          type="password"
+          name="password"
+          placeholder="Ваш пароль..."
+          required
+        />
+        <input
+          className={styles.formBlockInput}
+          type="password"
+          name="password-repeat"
+          placeholder="Повторите пароль..."
+          required
+        />
+        <button
+          className={styles.formBlockButton}
+          type="submit"
+        >
+          Зарегистрироваться
+        </button>
+      </form>
+
+      <div className={styles.fillBlock} />
+
+      <button
+        className={styles.formFutterButton}
+        type="button"
+        onClick={() => setIsAlreadyRegistered((prev) => !prev)}
+      >
+        Уже регистрировались?
+      </button>
     </div>
   );
 }
