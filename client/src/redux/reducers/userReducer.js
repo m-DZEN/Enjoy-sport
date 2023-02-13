4c1e64069f615f9f820219e6d3c55e53bdfc8078
Irina <irina.danilenko@gmail.com>
2023-02-13 12:09:04 +0300

    fix

--- client/src/redux/reducers/userReducer.js
+++ client/src/redux/reducers/userReducer.js
@@ -1,8 +1,7 @@
-const initialUserState = { userLogin: null, userId: null, userPoints: 0 };
+const initialUserState = { userLogin: null, userId: null, userName: null };
 
 const SET_USER_INFO = 'SET_USER_INFO';
 const CLEAR_USER_INFO = 'CLEAR_USER_INFO';
-const UPDATE_USER_POINTS = 'UPDATE_USER_POINTS';
 
 export function userReducer(state = initialUserState, action) {
   const { type, payload } = action;
@@ -10,9 +9,6 @@ export function userReducer(state = initialUserState, action) {
     case SET_USER_INFO:
       console.log({ type, payload });
       return { ...state, ...payload };
-    case UPDATE_USER_POINTS:
-      console.log({ type, payload });
-      return { ...state, userPoints: action.payload };
     case CLEAR_USER_INFO:
       console.log({ type, payload });
       return { ...state, ...initialUserState };
@@ -20,6 +16,5 @@ export function userReducer(state = initialUserState, action) {
       return state;
   }
 }
-export const setUserUpdatePoints = (payload) => ({ type: UPDATE_USER_POINTS, payload });
 export const setUserInfoAction = (payload) => ({ type: SET_USER_INFO, payload });
 export const clearUserInfoAction = () => ({ type: CLEAR_USER_INFO, payload: null });
