6124fc9497df773680f48936748f8ed1afaee7be
Max_V <mdzen87@gmail.com>
2023-02-14 22:29:57 +0300

    adminList

--- server/src/routes/adminListRoutes.js
+++ server/src/routes/adminListRoutes.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
-const { setUserList, deleteUser, addTrain } = require('../controllers/adminListControllers');
+const {
+  setUserList, deleteUser, addTrain, delTrain,
+} = require('../controllers/adminListControllers');
 
 module.exports = router.get('/', setUserList);
 module.exports = router.delete('/', deleteUser);
+module.exports = router.delete('/deltrain', delTrain);
 module.exports = router.post('/', addTrain);
