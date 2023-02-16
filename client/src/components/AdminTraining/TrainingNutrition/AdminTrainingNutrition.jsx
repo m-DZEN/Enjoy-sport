f72fbde3d42a9bbb6e36251fe821e5b08a552476
Irina <irina.danilenko@gmail.com>
2023-02-16 22:31:34 +0300

    adStyle2

--- client/src/components/AdminTraining/TrainingNutrition/AdminTrainingNutrition.jsx
+++ client/src/components/AdminTraining/TrainingNutrition/AdminTrainingNutrition.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
-import styles from './AdminTrainingNutrition.modules.scss';
+import styles from './AdminTrainingNutrition.module.scss';
 
 const typeFood = [
   { id: 1, title: 'Завтрак' },
@@ -99,7 +99,7 @@ export default function TrainingNutrition() {
           {typeFood.map((el) => (
             <>
               <tr key={el.id}>
-                <th className={styles.th} colSpan="5">{el.title}</th>
+                <th className={styles.day} colSpan="5">{el.title}</th>
               </tr>
               {nutrition.filter((e) => (e['DailyRecipe.Recipe.type_id'] === el.id)).map((e) => (
                 <tr key={e.id}>
@@ -108,11 +108,11 @@ export default function TrainingNutrition() {
                       {e['DailyRecipe.Recipe.title']}
                     </Link>
                   </th>
-                  <td>
+                  <td className={styles.td}>
                     {e['DailyRecipe.mass']}
                     гр.
                   </td>
-                  <td>
+                  <td className={styles.tdButton}>
                     <button type="button" className={styles.button} onClick={() => { delFood(e.id); }}>
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3" viewBox="0 0 16 16">
                         <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
@@ -125,12 +125,12 @@ export default function TrainingNutrition() {
           ))}
         </tbody>
       </table>
-      <table>
+      <table className={styles.table2}>
         <tbody>
-          <tr className="addForm" style={{ display: visible }}>
+          <tr className={styles.addForm} style={{ display: visible }}>
             <th>
               <select
-                className="selectform"
+                className={styles.thSelect}
                 onChange={formHandler}
                 name="type_id"
               >
@@ -141,13 +141,12 @@ export default function TrainingNutrition() {
             </th>
             <th>
               <input
-                type="number"
+                className={styles.thInput}
                 onChange={formHandler}
-                className="inputform"
                 name="mass"
               />
             </th>
-            <td>
+            <td className={styles.tdInputButton}>
               <button type="button" className={styles.button} onClick={saveFood}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-save" viewBox="0 0 16 16">
                   <path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v7.293l2.646-2.647a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L7.5 9.293V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1H2z" />
